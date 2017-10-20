7d8173ea6b919f5e17150ab36de8280bfd0b1572
Wickstrom <Wickstrom.kristoffer@gmail.com>
2017-10-20 15:06:17 +0200

    Update fcn16.py

--- models/fcn16.py
+++ models/fcn16.py
@@ -25,24 +25,24 @@ class FCN16(nn.Module):
         self.features4 = nn.Sequential(*features[0:34])                      # Split encoder into two parts to enable
         self.features5 = nn.Sequential(*features[34:])                       # skip connecntions.
         
-        for m in self.modules():                                             # 
-            if isinstance(m, nn.Conv2d):
+        for m in self.modules():                                             # Freeze weights of encoder to reduce amount of
+            if isinstance(m, nn.Conv2d):                                     # parameters needed to optimize.
                 m.require_grad = False
             
             if isinstance(m,nn.BatchNorm2d):
                 m.require_grad = False
      
-        self.fc = nn.Sequential(
-                nn.Conv2d(512,4096,kernel_size=7,padding=3),
-                nn.BatchNorm2d(4096),
-                nn.ReLU(inplace=True),
-                nn.Dropout(),
+        self.fc = nn.Sequential(                                             # Correspond to classifier in VGG16. Replace fully
+                nn.Conv2d(512,4096,kernel_size=7,padding=3),                 # connencted layers with convolutional layers. First
+                nn.BatchNorm2d(4096),                                        # convolution operation has padding = 3 to avoid
+                nn.ReLU(inplace=True),                                       # reducing the size of the input image to much.
+                nn.Dropout(),                                                # Dropout-rate is the standar 0.5.
                 nn.Conv2d(4096,4096,kernel_size=1),
                 nn.BatchNorm2d(4096),
                 nn.ReLU(inplace=True),
                 nn.Dropout(),
                 )
-        self.fc[0].weight = fcn_32.fc[0].weight
+        self.fc[0].weight = fcn_32.fc[0].weight                              # Import weights from trained FCN-32 model.
         self.fc[0].bias = fcn_32.fc[0].bias
         self.fc[1].weight = fcn_32.fc[1].weight
         self.fc[1].bias = fcn_32.fc[1].bias
@@ -51,29 +51,29 @@ class FCN16(nn.Module):
         self.fc[5].weight = fcn_32.fc[5].weight
         self.fc[6].bias = fcn_32.fc[5].bias
 
-        self.score_pool4 = nn.Conv2d(512,num_classes,kernel_size=1)   
-        nn.init.kaiming_normal(self.score_pool4.weight)
+        self.score_pool4 = nn.Conv2d(512,num_classes,kernel_size=1)          # Upsample by a factor of four and scoring   
+        nn.init.kaiming_normal(self.score_pool4.weight)                      # layer.
         nn.init.constant(self.score_pool4.bias,1)
         self.upsample_pool4 = nn.ConvTranspose2d(num_classes,num_classes,kernel_size=32,stride=16,padding=8,bias=False)
                   
-        self.score_pool5 = nn.Conv2d(4096,num_classes,kernel_size=1)   
-        self.score_pool5.weight = fcn_32._modules['score_pool5'].weight 
+        self.score_pool5 = nn.Conv2d(4096,num_classes,kernel_size=1)         # Upsample by a factor of two and
+        self.score_pool5.weight = fcn_32._modules['score_pool5'].weight      # scoring layer.
         self.score_pool5.bias = fcn_32._modules['score_pool5'].bias 
         self.upsample_pool5 = nn.ConvTranspose2d(num_classes,num_classes,kernel_size=4,stride=2,padding=1,bias=False)         
         
     def forward(self, x):
         
-        pool_4 = self.features4(x)
-        pool_5 = self.features5(pool_4)
-        fc = self.fc(pool_5) 
+        pool_4 = self.features4(x)                                           # Encoder up to fourth pooling layer.
+        pool_5 = self.features5(pool_4)                                      # Remaining part of encoder.
+        fc = self.fc(pool_5)                                                 # Altered classifier part.
         
-        score_pool5 = self.score_pool5(fc) 
+        score_pool5 = self.score_pool5(fc)                                   # Upsample end of encoder by a factor of two.
         upsample_pool5 = self.upsample_pool5(score_pool5)
         
-        score_pool4 = self.score_pool4(pool_4)
-        upsample_pool4 = self.upsample_pool4(upsample_pool5+score_pool4)
+        score_pool4 = self.score_pool4(pool_4)                               # Sum upsampled features and encoder features and
+        upsample_pool4 = self.upsample_pool4(upsample_pool5+score_pool4)     # upsample by a factor of four.
         
-        out = upsample_pool4.permute(1,0,2,3).contiguous()
+        out = upsample_pool4.permute(1,0,2,3).contiguous()                   # Modifiy shape for softmax.
         out = out.view(2,-1)
         out = out.permute(1,0)
 
