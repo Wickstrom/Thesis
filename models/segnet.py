b24501b8a2622825c4b6cb2dd1b625f3b6858fa7
Kristoffer <kwi030@post.uit.no>
2017-10-24 14:57:39 +0200

    Update Segnet

--- models/segnet.py
+++ models/segnet.py
@@ -2,6 +2,34 @@ import torch.nn as nn
 from torchvision import models
 import torch.nn.functional as F
 
+class Encoder(nn.Module):
+    def __init__(self,n_in, n_out,l,activation):
+        super(Encoder, self).__init__()
+        
+        self.l = l
+        self.n_in = n_in
+        self.n_out = n_out
+        self.activation = activation
+        
+        self.block = nn.Sequential(
+            *([nn.Conv2d(self.n_in, self.n_out, kernel_size=3, padding=1),
+               nn.BatchNorm2d(self.n_out),
+               self.activation,])
+        )
+        self.block_out = nn.Sequential(
+            *([nn.Conv2d(self.n_out, self.n_out, kernel_size=3, padding=1),
+               nn.BatchNorm2d(self.n_out),
+               self.activation]*l)
+        )
+        self.pool = nn.MaxPool2d(2,2,return_indices=True)
+    def forward(self,x):
+        
+        out = self.block(x)
+        out = self.block_out(out)
+        out, idx = self.pool(out)
+        
+        return out, idx
+
 class Decoder(nn.Module):
     def __init__(self,n_in, n_out,l,activation):
         super(Decoder, self).__init__()
@@ -21,43 +49,36 @@ class Decoder(nn.Module):
                nn.BatchNorm2d(self.n_out),
                self.activation])
         )
-    def forward(self,x):
-        
-        out = self.block(x)
+    
+        self.unpool = nn.MaxUnpool2d(2,2)
+    def forward(self,x,idx):
+
+        out = self.unpool(x,idx)        
+        out = self.block(out)
         out = self.block_out(out)
         
-        return(out)
-
+        return out
+    
 class SegNet(nn.Module):
     def __init__(self, num_classes,activation):
         super(SegNet, self).__init__()
-        features = list(models.vgg16_bn().features.children())
         
         self.activation = activation
         self.num_classses = num_classes
-        
-        for f in features:
-            if 'MaxPool' in f.__class__.__name__:
-                f.return_indices=True
 
-        self.enc1 = nn.Sequential(*features[0:7])
-        self.enc2 = nn.Sequential(*features[7:14])
-        self.enc3 = nn.Sequential(*features[14:24])
-        self.enc4 = nn.Sequential(*features[24:34])
-        self.enc5 = nn.Sequential(*features[40:])
-        
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                m.require_grad = False            
-            if isinstance(m, nn.BatchNorm2d):
-                m.require_grad = False      
+        self.enc1 = Encoder(3,64,1,self.activation)
+        self.enc2 = Encoder(64,128,1,self.activation)
+        self.enc3 = Encoder(128,256,2,self.activation)
+        self.enc4 = Encoder(256,512,2,self.activation)
+        self.enc5 = Encoder(512,512,2,self.activation)
                 
         self.dec5 = Decoder(512,512,2,self.activation)
         self.dec4 = Decoder(512,256,2,self.activation)
         self.dec3 = Decoder(256,128,2,self.activation)
         self.dec2 = Decoder(128,64,1,self.activation)
         self.dec1 = nn.Sequential(
-            *([nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            *([
+               nn.Conv2d(64, 64, kernel_size=3, padding=1),
                nn.BatchNorm2d(64),
                self.activation,
                nn.Conv2d(64, self.num_classses, kernel_size=3, padding=1)])
@@ -66,25 +87,15 @@ class SegNet(nn.Module):
     def forward(self, x):
         enc1, idx1 = self.enc1(x)
         enc2, idx2 = self.enc2(enc1)
-        enc3, idx3 = self.enc3(enc2)
+        enc3, idx3 = self.enc3(F.dropout2d(enc2))
         enc4, idx4 = self.enc4(F.dropout2d(enc3))
-        enc5, idx5 = self.enc5(F.dropout2d(enc4))
+        enc5, idx5 = self.enc5(F.dropout2d(enc4))       
         
-        dec5 = self.dec5(F.max_unpool2d(
-                         F.dropout2d(enc5),idx5,
-                         kernel_size=2,stride=2))
-        dec4 = self.dec4(F.max_unpool2d(
-                         F.dropout2d(dec5),idx4,
-                         kernel_size=2,stride=2))
-        dec3 = self.dec3(F.max_unpool2d(
-                         F.dropout2d(dec4),idx3,
-                         kernel_size=2,stride=2))
-        dec2 = self.dec2(F.max_unpool2d(
-                         F.dropout2d(dec3),idx2,
-                         kernel_size=2,stride=2))
-        dec1 = self.dec1(F.max_unpool2d(
-                         F.dropout2d(dec2),idx1,
-                         kernel_size=2,stride=2))
+        dec5 = self.dec5(F.dropout2d(enc5),idx5)
+        dec4 = self.dec4(F.dropout2d(dec5),idx4)
+        dec3 = self.dec3(F.dropout2d(dec4),idx3)
+        dec2 = self.dec2(dec3,idx2)
+        dec1 = self.dec1(F.max_unpool2d(dec2,idx1,2,2))
 
         out = dec1.permute(1,0,2,3).contiguous()
         out = out.view(2,-1)
