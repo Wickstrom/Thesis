6bfa6f0ddd0d267b00fc1b0023e71a4cf1523cdb
Wickstrom <Wickstrom.kristoffer@gmail.com>
2017-10-24 13:36:27 +0200

    Update Unet.py

--- models/Unet.py
+++ models/Unet.py
@@ -1,30 +1,35 @@
 import torch
-import numpy as np
 import torch.nn as nn
-from torchvision import models
 import torch.nn.functional as F
-from polyp_loader import transform
-from torch.autograd import Variable
 
-class Encoder(nn.Module):
+############################################################################################################
+# Implementation of U-net from https://arxiv.org/abs/1505.04597.                                           #
+# Note that this implementation assumes that input images have the property that                           #
+# modulo(Input H/W,5) = 0. This is to handle size information during the upsampling. The                   #
+# easiest way to ensure this is to either pad images and crop when calculating loss                        #
+# or crop incoming images to size (224,224) for example.                                                   #
+# Not the smoothest method to handle shape but i included this work as a part of data pre-processing.      #
+############################################################################################################
+
+class Encoder(nn.Module):                                                   
     def __init__(self, n_in,n_out,activation):
         super(Encoder, self).__init__()
         
-        self.n_in = n_in
-        self.n_out = n_out
+        self.n_in = n_in                                                   # Number of features in.
+        self.n_out = n_out                                                 # Number of features out.
         self.activation = activation
         
-        self.block = nn.Sequential(
+        self.block = nn.Sequential(                                        # Input encoder block.
             *([nn.Conv2d(self.n_in, self.n_out, kernel_size=3, padding=1),
                nn.BatchNorm2d(self.n_out),
                self.activation,])
         )
-        self.block_out = nn.Sequential(
+        self.block_out = nn.Sequential(                                    # Ouput encoder block. 
             *([nn.Conv2d(self.n_out, self.n_out, kernel_size=3, padding=1),
                nn.BatchNorm2d(self.n_out),
                self.activation])
         )
-    def forward(self,x):
+    def forward(self,x):                                                   # Forward pass for encoder.
         
         out = self.block(x)
         out = self.block_out(out)
@@ -35,24 +40,24 @@ class Decoder(nn.Module):
     def __init__(self, n_in,n_mid,n_out,activation):
         super(Decoder, self).__init__()
         
-        self.n_in = n_in
-        self.n_mid = n_mid
-        self.n_out = n_out
-        self.activation = activation
+        self.n_in = n_in                                                  # Number of features in.
+        self.n_mid = n_mid                                                # Number of features for center par of decoder.
+        self.n_out = n_out                                                # Number of features out of decoder.
+        self.activation = activation                                          
         
-        self.block = nn.Sequential(
+        self.block = nn.Sequential(                                       # Input decoder.
             *([nn.Conv2d(self.n_in, self.n_mid, kernel_size=3, padding=1),
                nn.BatchNorm2d(self.n_mid),
                self.activation,])
         )
-        self.block_out = nn.Sequential(
-            *([nn.Conv2d(self.n_mid, self.n_out, kernel_size=3, padding=1),
-               nn.BatchNorm2d(self.n_out),
+        self.block_out = nn.Sequential(                                    # Output decoder. Consists of conv layer, BN, activation
+            *([nn.Conv2d(self.n_mid, self.n_out, kernel_size=3, padding=1),# and transposed conv layer which upsamples by a factor
+               nn.BatchNorm2d(self.n_out),                                 # of two.
                self.activation,
                nn.ConvTranspose2d(self.n_out,self.n_out,
                                   kernel_size=4,stride=2,padding=1,bias=False)])
         )
-    def forward(self,x):
+    def forward(self,x):                                                  # Forward pass for decoder
         
         out = self.block(x)
         out = self.block_out(out)
@@ -70,8 +75,8 @@ class Unet(nn.Module):
         self.enc3 = Encoder(128,256,self.activation)
         self.enc4 = Encoder(256,512,self.activation)
         
-        self.center = nn.Sequential(
-            *([nn.Dropout2d(),
+        self.center = nn.Sequential(                                       # Center block has dropout included as 
+            *([nn.Dropout2d(),                                             # regularization, with a dropout-rate of 0.5.
                nn.Conv2d(512, 1024, kernel_size=3, padding=1),
                nn.BatchNorm2d(1024),
                self.activation,
@@ -87,8 +92,8 @@ class Unet(nn.Module):
         self.dec4 = Decoder(1024,512,256,self.activation)
         self.dec3 = Decoder(512,256,128,self.activation) 
         self.dec2 = Decoder(256,128,64,self.activation)    
-        self.dec1 = nn.Sequential(
-            *([nn.Conv2d(128,64, kernel_size=3, padding=1),
+        self.dec1 = nn.Sequential(                                         # Last part of decoder which outputs the number of  
+            *([nn.Conv2d(128,64, kernel_size=3, padding=1),                # classes.
                nn.BatchNorm2d(64),
                self.activation,
                nn.Conv2d(64,64, kernel_size=3, padding=1),
@@ -98,7 +103,7 @@ class Unet(nn.Module):
                ])
             )                
     
-    def forward(self,x):
+    def forward(self,x):                                                    
         
         enc1 = self.enc1(x)
         enc2 = self.enc2(F.max_pool2d(enc1,kernel_size=2,stride=2))
@@ -112,8 +117,8 @@ class Unet(nn.Module):
         dec2 = self.dec2(torch.cat([dec3,enc2],1))           
         dec1 = self.dec1(torch.cat([dec2,enc1],1))  
         
-        out = dec1.permute(1,0,2,3).contiguous()
-        out = out.view(2,-1)
+        out = dec1.permute(1,0,2,3).contiguous()                           # Rearranging output into size 
+        out = out.view(2,-1)                                               # (Number of samples, Number of classes) for softmax func.                                          
         out = out.permute(1,0)
 
         return out
